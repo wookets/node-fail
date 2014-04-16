1e7a545c15733c0afaa722804f5e8ef1a8fff483
Sean Wesenberg <wookets@gmail.com>
2014-04-16 11:24:57 -0500

    0.2.0 - added specific http errors
    Signed-off-by: Sean Wesenberg <wookets@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 
 
-module.exports = function(name, message) {
+exports.BadRequest = function(name, message) {
   var err = new Error(message);
-  err.name = name;
-  err.fail = true;
+  err.name = 'BadRequest';
+  err.status = 400;
+  err.data = data;
   return err;
-}
+};
+
+exports.NotAuthorized = function(name, message) {
+  var err = new Error(message);
+  err.name = 'NotAuthorized';
+  err.status = 400;
+  err.data = data;
+  return err;
+};
+
+exports.NotFound = function(message, data) {
+  var err = new Error(message);
+  err.name = 'NotFound';
+  err.status = 404;
+  err.data = data;
+  return err;
+};
+
+exports.InternalServerError = function(message, data) {
+  var err = new Error(message);
+  err.name = 'InternalServerError';
+  err.status = 500;
+  err.data = data;
+  return err;
+};
